392e6e3bceab2c2488ba7935ccc313d23a682703
preritjn <preritbgr3010@gmail.com>
2022-06-13 00:54:49 +0530

    created a python folder

---
+++ Python/practice.py
@@ -0,0 +1,17 @@
+# Add two numbers
+
+
+# adding two integers
+a=input("Enter a number : ")
+a=int(a)
+b=input("Enter a number : ")
+b=int(b)
+print(a+b)
+
+# remainder
+print("The remainder is : ",a%b)
+ 
+print(a>b) 
+avg=(a+b)/2
+print("The value of average is : ",avg)
+print((a+b)/2)
