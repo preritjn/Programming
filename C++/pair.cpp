046ddf7270c93acc4031cea1d43c07e0dbc0cffd
preritjn <preritbgr3010@gmail.com>
2022-06-22 22:50:03 +0530

    added some lines

--- C++/pair.cpp
+++ C++/pair.cpp
@@ -1,8 +1,13 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 int main()
 {
-    
+    pair<int, string> a;
+    a = make_pair(11, "Prerit");
+    cout << a.first << " " << a.second;
+    a = {011, "Prerit Kumar Jain"};
+    cout << a.first << " " << a.second;
+
     return 0;
 }
