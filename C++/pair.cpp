1eb7357ea7c05dbf19613f86bdcd374bcb590a81
preritjn <preritbgr3010@gmail.com>
2022-06-22 23:00:31 +0530

    added some lines

--- C++/pair.cpp
+++ C++/pair.cpp
@@ -5,9 +5,19 @@ int main()
 {
     pair<int, string> a;
     a = make_pair(11, "Prerit");
-    cout << a.first << " " << a.second;
+    cout << a.first << " " << a.second <<endl;
     a = {011, "Prerit Kumar Jain"};
     cout << a.first << " " << a.second;
+
+    // array elements swapping
+    int a[]={1,2,3};
+    int b[]={2,3,4};
+    pair<int,int> p[3];
+    p[0]={1,2};
+    p[1]={2,3};
+    p[2]={3,4};
+    
+
 
     return 0;
 }
