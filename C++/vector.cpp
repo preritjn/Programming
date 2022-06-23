0c490ec19eab30d7eca1ca96000cfb3b0dcd9b80
preritjn <preritbgr3010@gmail.com>
2022-06-24 02:05:46 +0530

    added more lines

--- C++/vector.cpp
+++ C++/vector.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+void printVec(vector<int> v)
+{
+    cout << "Size " << v.size() << endl;
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+}
 
 int main()
 {
-    vector<int> v;  // dynamic array
-    int n,x;
-    for(int i=0;i<n;i++)
+    vector<int> v; // dynamic array
+    int n, x;
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        cin>>x;
+        cin >> x;
         v.push_back(x);
     }
+    printVec(v);
 }
