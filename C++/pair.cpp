c783325c4c0fc569c8f4fbf44deaf1e0d074d501
preritjn <preritbgr3010@gmail.com>
2022-06-23 22:53:13 +0530

    added more lines

--- C++/pair.cpp
+++ C++/pair.cpp
@@ -5,26 +5,26 @@ int main()
 {
     pair<int, string> a;
     a = make_pair(11, "Prerit");
-    cout << a.first << " " << a.second <<endl;
+    cout << a.first << " " << a.second << endl;
     a = {011, "Prerit Kumar Jain"};
-    cout << a.first << " " << a.second <<endl;
-    pair<int,string> a1;
-    cin>> a1.first>> a1.second;
-    cout << a1.first <<" "<<a1.second<<endl;
+    cout << a.first << " " << a.second << endl;
+    pair<int, string> a1;
+    cin >> a1.first >> a1.second;
+    cout << a1.first << " " << a1.second << endl;
 
     // array elements swapping
-    int c[]={1,2,3};
-    int b[]={2,3,4};
-    pair<int,int> p[3];
-    p[0]={1,2};
-    p[1]={2,3};
-    p[2]={3,4};
-    cout<<"Before Swapping\n";
-    for(int i=0;i<3;i++)
-    cout<<p[i].first << " "<<p[i].second<<endl;  
-    swap(p[0],p[2]);
-    cout<<"After Swapping\n";
-    for(int i=0;i<3;i++)
-    cout<<p[i].first << " "<<p[i].second<<endl;   
+    int c[] = {1, 2, 3};
+    int b[] = {2, 3, 4};
+    pair<int, int> p[3];
+    p[0] = {1, 2};
+    p[1] = {2, 3};
+    p[2] = {3, 4};
+    cout << "Before Swapping\n";
+    for (int i = 0; i < 3; i++)
+        cout << p[i].first << " " << p[i].second << endl;
+    swap(p[0], p[2]);
+    cout << "After Swapping\n";
+    for (int i = 0; i < 3; i++)
+        cout << p[i].first << " " << p[i].second << endl;
     return 0;
 }
