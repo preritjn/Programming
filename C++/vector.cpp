773d54dcb333e9e06fa111f9c883f9e0511537c9
preritjn <preritbgr3010@gmail.com>
2022-06-24 01:51:56 +0530

    added more lines

---
+++ C++/vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;  // dynamic array
+    int n,x;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        v.push_back(x);
+    }
+}
