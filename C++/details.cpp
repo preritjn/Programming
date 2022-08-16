35e8fe64b7648b0b0a4ea58e15b308f7bbcda062
preritjn <preritbgr3010@gmail.com>
2022-08-16 22:37:40 +0530

    added some files

---
+++ C++/details.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Name: Prerit Kumar Jain \nRegd No. 2102040011 \nBranch: Computer Science & Engineering \nAddress: Balangir";
+    return 0;
+}
