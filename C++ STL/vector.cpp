de70d7d8985624420cddb7abf75bebd1e644ac87
preritjn <preritbgr3010@gmail.com>
2022-09-03 12:26:49 +0530

    Done some changes in vectors and pair

--- C++ STL/vector.cpp
+++ C++ STL/vector.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 //push_back add element at last index
 // pop_back remove element at last index
+// both are 0(1) time complexity
+// pass reference rather than copy to reduce time compelxity
+// initializing vector with another vector has time complexity of 0(n)
 
-void printVec(vector<int> v)
+void printVec(vector<int> &v)
 {
     cout << "Size " << v.size() << endl;
     for (int i = 0; i < v.size(); i++)
         cout << v[i] << " ";
     cout << endl;
+    v.push_back(2);
 }
 
 int main()
@@ -22,8 +25,17 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> x;
-        printVec(v);
         v.push_back(x);
+        printVec(v);
     }
+    v.push_back(6);
+    v.push_back(9);
+    vector<int> v2 =v;
     printVec(v);
+    printVec(v);
+    v2.push_back(42);
+    printVec(v2);
+    printVec(v2);
+    
+    return 0;   
 }
