da4db3d46d936c5051d64997e40f737406972a6d
preritjn <preritbgr3010@gmail.com>
2022-09-21 02:03:01 +0530

    Added a new file

---
+++ C++ STL/unorderedmap_ques.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    unordered_map<string,int> m;
+    int n;
+    cin>>n;
+    while(n--) {
+        string s;
+        cin>>s;
+        m[s]++;
+    }
+    int q;
+    cin>>q;
+    while(q--) {
+        string s;
+        cin>>s;
+        cout<<m[s]<<endl;
+    }
+    return 0;
+}
