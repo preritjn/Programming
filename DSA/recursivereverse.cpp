a17696a0cfca92251712cc9ff4a24618625f1da4
preritjn <preritbgr3010@gmail.com>
2022-10-15 01:25:47 +0530

    Added a file

---
+++ DSA/recursivereverse.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+    Node(int n) {
+        data = n;
+        next = NULL;
+    }
+};
+
+Node *insertBegin(Node *head,int n) {
+    Node *temp = new Node(n);
+    if(head == NULL)
+        return temp;
+    temp->next = head;
+    return temp;
+}
+
+Node *insertEnd(Node *head,int n) {
+    Node *temp = new Node(n);
+    if(head == NULL)
+        return temp;
+    Node *curr = head;
+    while(curr->next != NULL)
+        curr = curr->next;
+    curr->next = temp;
+    return head;
+}
+
+void printList(Node *head) {
+    if(head == NULL)
+        return;
+    cout<<head->data<<" "<<head->next<<endl;
+    printList(head->next);
+}
+
+Node *revRecur(Node *head) {
+    if(head == NULL || head->next == NULL) 
+        return head;
+    Node *restHead = revRecur(head->next);
+    Node *restTail = head->next;
+    restTail->next = head;
+    head->next = NULL;
+    return restHead;
+}
+
+int main() {
+    Node *head = NULL;
+    head = insertBegin(head,50);
+    head = insertBegin(head,40);
+    head = insertBegin(head,30);
+    head = insertBegin(head,20);
+    head = insertBegin(head,10);
+    printList(head);
+    head = insertEnd(head,60);
+    head = insertEnd(head,70);
+    head = insertEnd(head,80);
+    head = insertEnd(head,90);
+    head = insertEnd(head,100);
+    printList(head);
+    head = revRecur(head);
+    printList(head);
+    return 0;
+}
