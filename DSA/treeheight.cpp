ce506046e81375975ce950101250da7d005334f6
preritjn <preritbgr3010@gmail.com>
2022-11-15 01:27:13 +0530

    Added a cpp file

---
+++ DSA/treeheight.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+    Node(int n) {
+        key = n;
+        left = right = NULL;
+    }
+};
+
+int height(Node *root) {
+    if(root == NULL)
+        return 0;
+    return max(height(root->left),height(root->right))+1;
+}
+
+int main() {
+    Node *root = new Node(10);
+    Node *root1 = new Node(20);
+    Node *root2 = new Node(30);
+    Node *root3 = new Node(40);
+    Node *root4 = new Node(50);
+    root->left = root1;
+    root->right = root2;
+    root->right->left = root3;
+    root->right->right = root4;
+    int h = height(root);
+    cout<<h;
+    return 0; 
+}
