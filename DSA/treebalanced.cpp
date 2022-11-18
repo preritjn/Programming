25d0a11a2e55018bdec443163378d9016fa8aff7
Prerit Kumar Jain <96645225+preritjn@users.noreply.github.com>
2022-11-18 16:27:53 +0530

    Done some changes

--- DSA/treebalanced.cpp
+++ DSA/treebalanced.cpp
@@ -14,7 +14,7 @@ struct Node {
 int height(Node *root) {
     if(root == NULL)
         return 0;
-    return max(height(root->left),height(root->right)+1);
+    return max(height(root->left),height(root->right))+1;
 }
 
 bool isBalanced(Node *root) {
