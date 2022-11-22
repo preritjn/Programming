16b7fc983802de3d7e0562d8d3c4c07d7a7f1d9b
preritjn <preritbgr3010@gmail.com>
2022-11-22 11:14:49 +0530

    Done some changes

--- DSA/treetoinorderpreorder.cpp
+++ DSA/treetoinorderpreorder.cpp
@@ -16,12 +16,11 @@ Node *cTree(int in[],int pre[],int is,int ie) {
         return NULL;
     Node *root = new Node(pre[preIndex++]);
     int inIndex;
-    for(int i = is;i<=ie;i++) {
-        if(in[i]== root->data) {
+    for(int i = is;i<=ie;i++) 
+        if(in[i]==root->data) {
             inIndex = i;
             break;
         }
-    }
     root->left = cTree(in,pre,is,inIndex-1);
     root->right = cTree(in,pre,inIndex+1,ie);
     return root;
@@ -36,9 +35,9 @@ void inOrder(Node *root) {
 }
 
 int main() {
-    int in[] = {20,10,40,30,50};
-    int pre[] = {10,20,30,40,50};
-    Node *root = cTree(in,pre,0,4);
+    int in[] = {40,20,60,50,70,10,80,100,30};
+    int pre[] = {10,20,40,50,60,70,30,80,100};
+    Node *root = cTree(in,pre,0,8);
     inOrder(root);
     return 0;
 }
