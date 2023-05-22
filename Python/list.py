445d6d3af6d77d11a803f845a4e5568cdfb9135c
preritjn <preritbgr3010@gmail.com>
2023-05-22 18:36:43 +0530

    Added some files

--- Python/list.py
+++ Python/list.py
@@ -15,15 +15,36 @@ lists.pop(2)
 print(lists)
 lists.remove(25)
 print(lists)
-
+# tuples are immutable
 t1 = (1, 2, 3, 5, 4, 6, 3, 3, 3)
 print(t1)
 print(t1[0])
 
-t2 = ()  # Empty tuple
+# Empty tuple
+t2 = ()
+empty_tuple = tuple()
 # t3=(1) wrong way to declare single element tuple
 t3 = (1,)  # single element tuple
 print(t2)
 print(t3)
 print(t1.count(3))
 print(t1.index(4))
+
+new_list = ['History', 'Math', 'Physics', 'Chemistry', 'Biology']
+sorted_new_list = sorted(new_list)
+print(new_list)
+print(sorted_new_list)
+
+for course in new_list:
+    print(course)
+for index, course in enumerate(sorted_new_list):
+    print(index, course)
+
+new_list_str = " - ".join(new_list)
+new_list1 = new_list_str.split(" - ")
+print(new_list_str)
+print(new_list1)
+
+# empty list
+empty_list = []
+empty_list = list()
---
+++ Python/sets.py
@@ -0,0 +1,13 @@
+cs_course = {'History', 'Maths', 'Physics', 'CompSci'}
+art_course = {'History', 'Maths', 'Design', 'Art'}
+
+print(cs_course)
+print(art_course)
+print('Maths' in cs_course)
+print(cs_course.intersection(art_course))
+print(cs_course.difference(art_course))
+print(cs_course.union(art_course))
+
+
+# empty set
+empty_set = set()
