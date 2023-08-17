a27845cb4d12b58536b8da8fb64a4ee4c3340e42
preritjn <preritbgr3010@gmail.com>
2023-08-17 14:02:33 +0530

    Added some files

--- Python/task.py
+++ Python/task.py
@@ -5,7 +5,7 @@ import pywhatkit
 # pywhatkit.sendwhatmsg("+917665127830", "Hello bhaiya", 11, 29)
 
 pywhatkit.sendwhatmsg_instantly(
-phone_no="+918260677797", 
-message="Hello",
+phone_no="+919827622897", 
+message="Hello This is regarding your loc certificate! you gave the wrong mail id",
 )
 
