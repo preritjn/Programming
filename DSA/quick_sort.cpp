57d49fdd71dc4f9976baf10c23006e2491260cb1
preritjn <preritbgr3010@gmail.com>
2024-01-26 02:10:37 +0530

    Added a file

--- DSA/quick_sort.cpp
+++ DSA/quick_sort.cpp
@@ -3,7 +3,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int partition(vector<int> &arr,int low,int high) {
+    int i=low,j=high;
+    int pivot = arr[low];
+    while(i<j) {
+        while(pivot >= arr[i] && i <= high-1)
+            i++;
+        while(pivot < arr[j] && j >= low+1)
+            j--;
+        if(i<j)
+            swap(arr[i],arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+
+void quickSort(vector<int> &arr,int low,int high) {
+    if(low<high) {
+        int pivot = partition(arr,low,high); 
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+    }
+}
+
 int main() {
-    
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++) {
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    quickSort(arr,0,n-1);
+    for(auto val : arr)
+        cout<<val<<" ";
     return 0;
 }
